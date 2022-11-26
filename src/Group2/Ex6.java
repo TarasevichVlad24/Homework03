5d481f71d4bf922c7f3868c09c6731019e243139
TarasevichVlad24 <gagagdet@gmail.com>
2022-11-26 18:49:39 +0200

    Ex6(2)

---
+++ src/Group2/Ex6.java
@@ -0,0 +1,9 @@
+package Group2;
+
+public class Ex6 {
+    public static void main(String [] args){
+
+        for (int a = 10; a <= 20; a++)
+            System.out.println(a + "^2" + "=" + a * a);
+    }
+}
