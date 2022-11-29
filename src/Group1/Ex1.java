56f855c8e41b66a15207424c22cfb4aef58c627c
TarasevichVlad24 <gagagdet@gmail.com>
2022-11-29 18:33:47 +0200

    Ex1

--- src/Group1/Ex1.java
+++ src/Group1/Ex1.java
@@ -3,52 +3,19 @@ package Group1;
 import java.util.Scanner;
 
 public class Ex1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int a;
         Scanner s = new Scanner(System.in);
         System.out.print("Введите номер месяца:  ");
         a = s.nextInt();
 
-        switch(a){
-            case 1:
-                System.out.println("Зима");
-                break;
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 3:
-                System.out.println("Весна");
-                break;
-            case 4:
-                System.out.println("Весна");
-                break;
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-                System.out.println("Лето");
-                break;
-            case 7:
-                System.out.println("Лето");
-                break;
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-                System.out.println("Осень");
-                break;
-            case 10:
-                System.out.println("Осень");
-                break;
-            case 11:
-                System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
-                break;
-            default:
-                System.out.println(a + "-го месяца не существует");
+        switch (a) {
+            case 1, 2, 12 -> System.out.println("Зима");
+            case 3, 5, 4 -> System.out.println("Весна");
+            case 6, 7, 8 -> System.out.println("Лето");
+            case 9, 10, 11 -> System.out.println("Осень");
+            default -> System.out.println(a + "-го месяца не существует");
         }
     }
 }
